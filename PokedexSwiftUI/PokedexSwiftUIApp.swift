954b281fd246d4c6398a89c10442412ef7b2c5f3
ZarEAhmer <zare.ahmer@appinsnap.com>
2024-07-08 00:09:07 -0700

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Macbook on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
